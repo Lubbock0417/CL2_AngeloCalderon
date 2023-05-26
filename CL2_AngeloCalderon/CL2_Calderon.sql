81bf722a2d9522f9226ff4788340789913d947a2
Angelo Calderon Cornejo <I202117357@cibertec.edu.pe>
2023-05-25 22:13:27 -0500

    Ultimos cambios

--- CL2_AngeloCalderon/CL2_Calderon.sql
+++ CL2_AngeloCalderon/CL2_Calderon.sql
@@ -12,21 +12,21 @@ CREATE TABLE Area (
     jefe_area VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE Personal (
+  CREATE TABLE Personal (
     id INT PRIMARY KEY,
     nombre VARCHAR(255) NOT NULL,
     codigo_trabajador VARCHAR(255) NOT NULL,
     sueldo DOUBLE NOT NULL,
-    codigo_area INT,
-    FOREIGN KEY (codigo_area) REFERENCES Area(codigo)
+    codigo INT,
+    FOREIGN KEY (codigo) REFERENCES Area(codigo)
 );
-
+   
 INSERT INTO Area (codigo, nombre_area, jefe_area) VALUES
     (1, 'CONTABILIDAD', 'Pedro Pascal'),
     (2, 'VENTAS', 'Ivan Rakitich'),
     (3, 'MARKETING', 'Bernardo Cuesta');
 
-INSERT INTO Personal (id, nombre, codigo_trabajador, sueldo, codigo_area) VALUES
+INSERT INTO Personal (id, nombre, codigo_trabajador, sueldo, codigo) VALUES
     (1, 'Lionel Messi', 'COD1', 2000.00, 1),
     (2, 'Kevin De Bryune', 'COD2', 1800.00, 1),
     (3, 'Jack Nicholson', 'COD3', 2200.00, 2),
@@ -35,7 +35,7 @@ INSERT INTO Personal (id, nombre, codigo_trabajador, sueldo, codigo_area) VALUES
     (6, 'Freddy Sinna', 'COD6', 2400.00, 3),
     (7, 'Luis Iberico', 'COD7', 2300.00, 3),
     (8, 'Ricardo Centeno', 'COD8', 2500.00, 3);
-
+    
 SELECT p.*, a.jefe_area
 FROM Personal p
 INNER JOIN Area a ON p.codigo = a.codigo;
